5809c413cf7b8c0948de588b37626b433086754b
adeenazara2002 <adinazara2001@gmail.com>
2024-08-30 20:22:27 +0500

    Home Screen

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -13,106 +13,31 @@ class Home extends StatelessWidget {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromRGBO(28, 37, 70, 1.0), 
-              Color.fromRGBO(40, 46, 93, 1.0), 
-              Color.fromRGBO(57, 56, 124, 1.0), 
-              Color.fromRGBO(63, 56, 134, 1.0), 
-              Color.fromRGBO(74, 60, 148, 1.0), 
-              Color.fromRGBO(67, 59, 141, 1.0), 
-              Color.fromRGBO(84, 63, 158, 1.0), 
-              Color.fromRGBO(108, 74, 171, 1.0), 
-              Color.fromRGBO(121, 80, 172, 1.0), 
-              Color.fromRGBO(135, 75, 171, 1.0), 
-
-
+              Color.fromRGBO(28, 37, 70, 1.0),
+              Color.fromRGBO(40, 46, 93, 1.0),
+              Color.fromRGBO(57, 56, 124, 1.0),
+              Color.fromRGBO(63, 56, 134, 1.0),
+              Color.fromRGBO(74, 60, 148, 1.0),
+              Color.fromRGBO(67, 59, 141, 1.0),
+              Color.fromRGBO(84, 63, 158, 1.0),
+              Color.fromRGBO(108, 74, 171, 1.0),
+              // Color.fromRGBO(121, 80, 172, 1.0),
+              // Color.fromRGBO(135, 75, 171, 1.0),
             ],
           ),
         ),
         child: Column(
           children: [
             SizedBox(
-              height: 180,
+              height: 300,
             ),
             Row(
               children: [
-                Padding(padding: EdgeInsets.only(left: 25)),
-                Image.asset('assets/images/cloud.png'),
+                Padding(padding: EdgeInsets.only(left: 60)),
+                Image.asset('assets/images/house.png'),
               ],
             ),
-            // === TEXT ROW ===
-            SizedBox(
-              height: 20,
-            ),
-            Stack(
-              children: [
-                Row(
-                  children: [
-                    Padding(padding: EdgeInsets.only(left: 80)),
-                    Text(
-                      'Weather',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ],
-                ),
-                // === TEXT 2 ROW ===
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: 50, left: 80), // Adjust this for desired space
-                  child: Text(
-                    'Forecast',
-                    style: TextStyle(
-                      color: Color.fromRGBO(221, 177, 48, 1.0),
-                      fontSize: 50,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            // === BUTTON ===
-
-            SizedBox(
-              height: 30,
-            ),
-
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(left: 40),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Home(),
-                          ));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(221, 177, 48, 1.0),
-                      foregroundColor: Color.fromRGBO(72, 60, 146, 1.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 70.0, vertical: 18.0),
-                      child: Text(
-                        'Get Start',
-                        style: TextStyle(
-                            fontSize: 24.0, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
+                     ],
         ),
       ),
     );
